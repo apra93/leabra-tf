9e066d8690c6a5afc03915dc3eaf756cda317f85
apra <dd.apra93@gmail.com>
2019-02-17 18:23:01 -0500

    ENH: Migrated visualization routines from nb-0.1.1 and added a basic test.

--- leabratf/tests/test_combigen_heatmap.py
+++ leabratf/tests/test_combigen_heatmap.py
@@ -6,8 +6,15 @@ import numpy as np
 
 from leabratf.visualization import combigen_heatmap as ch
 
-@pytest.mark.parametrize("length", [1, 3, 10, 20])
+N = [1, 3, 10]
+
+@pytest.mark.parametrize("length", N)
 def test_combigen_heatmap_runs_without_errors_for_different_input_lengths(
         length):
     """Lifted directly from nb0.1 c382."""
     ch.heatmap(np.random.choice(2, (length,5,2), True))
+
+@pytest.mark.parametrize("n_pairs", N)
+def test_visualize_combigen_runs_without_errors_for_different_numbers_of_pairs(
+        n_pairs):
+    ch.visualize_combigen(n_pairs)
--- leabratf/visualization/combigen_heatmap.py
+++ leabratf/visualization/combigen_heatmap.py
@@ -1,9 +1,11 @@
 """Visualization code for the combigen task."""
 import logging
+from itertools import count
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import leabratf.tasks.combinatorics.combigen as cg
 from leabratf.utils import make_input_3d
 
 logger = logging.getLogger(__name__)
@@ -12,7 +14,7 @@ logger = logging.getLogger(__name__)
 @make_input_3d
 def heatmap(data, vmin=0, vmax=2, cbar=False, linewidths=1, square=True, 
             samples_per_row=10, gridspec_kw=None, sharey=True, sharex=True,
-            *args, **kwargs):
+            titles=None, *args, **kwargs):
     """Wrapper function of `sns.heatmap` with some different defaults. 
     
     Only changed values are shown, see the documentation for `sns.heatmap`
@@ -56,10 +58,18 @@ def heatmap(data, vmin=0, vmax=2, cbar=False, linewidths=1, square=True,
     hor_size = (samples_per_row 
                 if (ver_size > 1 or n_samples == samples_per_row) 
                 else n_samples % samples_per_row)
-    
+    # Create the subplot axes
     _, axn = plt.subplots(ver_size, hor_size, sharey=sharey, sharex=sharex,
                           squeeze=False, gridspec_kw=gridspec_kw)
-    
+
+    # Create a titles generator
+    if titles is not None:
+        titles = iter(titles)
+    else:
+        titles = count(start=0, step=1)
+    # Collect the returned heatmap parameters
+    heatmaps = []
+
     # Loop through and generate the plots
     gen_data = iter(data)
     for i in range(ver_size):
@@ -69,9 +79,32 @@ def heatmap(data, vmin=0, vmax=2, cbar=False, linewidths=1, square=True,
                 shape = array.shape
                 if len(shape) == 3 and shape[0] == 1:
                     array = array.reshape(shape[1:])
-                sns.heatmap(array, vmin=vmin, vmax=vmax, cbar=cbar,
-                            linewidths=linewidths, square=square, ax=axn[i,j], 
-                            *args, **kwargs)
-                axn[i,j].set_title(i*ver_size + j)
+                heatmaps.append(sns.heatmap(
+                    array, vmin=vmin, vmax=vmax, cbar=cbar,
+                    linewidths=linewidths, square=square, ax=axn[i,j], *args,
+                    **kwargs))
+                axn[i,j].set_title(next(titles))
         except StopIteration:
             break
+    return heatmaps
+
+def visualize_combigen(n_pairs=1, *args, **kwargs):
+    """Plot N x and y pairs of the combigen task
+
+    Parameters
+    ----------
+    n_pairs : int, optional
+    	The number of X, y pairs to visualize"""
+    heatmaps = []
+    # Visulize a few combinations of x and y
+    for _ in range(n_pairs):
+        # Generate a signle y
+        example_y = cg.generate_labels(n_samples=1, *args, **kwargs)
+        # Generate a single x from the y
+        example_x = cg.inverse_transform(example_y)
+        heatmaps.append(heatmap([example_y, example_x[0]],
+                                gridspec_kw={'width_ratios': [2, 5]},
+                                sharex=False,
+                                titles=['y', 'X']))
+    return heatmaps
+        
