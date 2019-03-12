38751569b52e9ed59dc755d7fbd65adc9cb091fd
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-03-12 05:26:41 +0000

    Update docs after building Travis build 70 of apra93/leabra-tf
    The docs were built from the branch 'master' against the commit
    76a0ccc39b6441bd34b5f4a70d5758a9b6907492.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/apra93/leabra-tf/jobs/505059620.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/_build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.71.gd2b8448',
+    VERSION: '0+untagged.68.g76a0ccc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
