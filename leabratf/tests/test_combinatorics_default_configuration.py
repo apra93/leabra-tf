fa123f343a26d6fa4d915de785fcb74aadfdcb3d
apra <dd.apra93@gmail.com>
2019-04-11 13:31:00 -0400

    TST: Adding tests for the new default config.

---
+++ leabratf/tests/test_combinatorics_default_configuration.py
@@ -0,0 +1,15 @@
+"""Tests for the default combinatorics """
+import logging
+
+logger = logging.getLogger(__name__)
+
+def test_combinatorial_default_configuration_imports():
+    import leabratf.tasks.combinatorics.default_configuration as config
+
+def test_combinatorial_default_configuration_attrs():
+    import leabratf.tasks.combinatorics.default_configuration as config
+    # Loop through each attribute given by dir and simply call it
+    for attr in dir(cfg):
+        # Skip if its a dunder variable
+        if '__' not in attr:
+            getattr(cfg, attr)    
