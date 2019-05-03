21945f089d88e957b2cdfc4e1302160ecbd79dea
apra <dd.apra93@gmail.com>
2019-05-03 16:47:11 -0400

    nb-0.5.1: Finally found the issue. Moral - use float32s.

--- leabratf/tasks/combinatorics/default_configuration.py
+++ leabratf/tasks/combinatorics/default_configuration.py
@@ -1,6 +1,7 @@
 """File to store the default experimental configuration."""
-
 import logging
+from types import ModuleType
+from pstar import pdict
 
 logger = logging.getLogger(__name__)
 
@@ -27,6 +28,8 @@ n_train = 100
 n_val = 50
 # Number of samples in the testing set
 n_test = 500
+# General variable that is meant to change based on the context
+n_samples = n_train
 
 # Network parameters
 
@@ -51,3 +54,50 @@ tf_device = '/cpu:0'
 n_models = 10
 # Recompute the model accuracy after this many epochs
 n_epochs_acc = 25
+
+# Meta data variables
+# Config name
+_name = 'default'
+# This is a configuration dict
+_config = True
+
+def default_config(**kwargs):
+    """Function that turns all the values defined above into a dictionary.
+
+    Grabs all the values in the ``globals`` dictionary and adds them to the
+    returned dictionary if it is a relevant variable.
+
+    The function also accepts keyword arguments that will be added to the config
+    at the end, overwriting the existing value, or adding the key/value pair.
+
+    Returns
+    -------
+    default_config : pdict
+    	Default configuration as defined by in the default_configuration.py file
+    """
+    # Configuration in a dictionary format
+    locals_dict = globals()
+    # Empty dict we will fill
+    _dict = pdict()
+
+    # Remove all the non configuration 
+    for key, val in locals_dict.items():
+        # Skip if the key is a dunder key
+        if key.startswith('__') and key.endswith('__'):
+            continue
+        # Skip if the value is a module
+        if isinstance(val, ModuleType):
+            continue
+        # Skip if the value is a callable (presumably a function)
+        if callable(val):
+            continue
+        # Skip if the value is the logger
+        if val is logger:
+            continue
+
+        # Passed all skips, add to the dict
+        _dict[key] = val
+
+    # Add any kwargs to the dictionary, overwriting any conflicting values
+    _dict.update(**kwargs)
+    return _dict
